5c462dec9ffd78dda284bc419bbefde1f8277323
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-07-07 21:20:09 +0530

    SQL updated

--- ezfood.sql
+++ ezfood.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2019 at 05:18 PM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.3.6
+-- Generation Time: Jul 07, 2019 at 05:49 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -106,6 +106,7 @@ CREATE TABLE `shops` (
 
 CREATE TABLE `user` (
   `userid` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
   `firstname` varchar(20) NOT NULL,
   `lastname` varchar(20) NOT NULL,
   `mobileno` int(12) NOT NULL,
@@ -113,6 +114,13 @@ CREATE TABLE `user` (
   `password` varchar(16) NOT NULL,
   `usertype` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `username`, `firstname`, `lastname`, `mobileno`, `email`, `password`, `usertype`) VALUES
+(1, 'dsdd', 'ddsdd', 'dsd', 0, 'dsd', 'sd', 'customer');
 
 --
 -- Indexes for dumped tables
@@ -192,7 +200,7 @@ ALTER TABLE `shops`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
