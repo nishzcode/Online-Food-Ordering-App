9178b6f78927bb17209ecde89aa249604ea37d15
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-07-07 21:10:52 +0530

    function added regiter

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component , OnInit} from '@angular/core';
 import { NavController, AlertController } from '@ionic/angular';
 import { AuthServiceService } from '../../app/auth-service.service';
+import { UserService } from '../api/user.service';
 
 @Component({
   selector: 'app-register',
@@ -10,24 +11,24 @@ import { AuthServiceService } from '../../app/auth-service.service';
 export class RegisterPage implements OnInit {
 
   createSuccess = false;
-  registerCredentials = { firstname: '', lastname: '', mobileno: '', username: '' ,password: '' }
+  registerCredentials = { firstname: '', lastname: '', mobileno: '', username: '' , password: '' };
 
-  constructor(/*private alertCtrl: AlertController, private auth: AuthServiceService ,private nav: NavController*/) { }
+  constructor(private alertCtrl: AlertController, private auth: UserService) { }
 
   ngOnInit() {
   }
-/*
+
   public register() {
     this.auth.register(this.registerCredentials).subscribe(success => {
       if (success) {
         this.createSuccess = true;
-        this.showPopup("Success", "Account created.");
+        this.showPopup('Success', 'Account created.');
       } else {
-        this.showPopup("Error", "Problem creating account.");
+        this.showPopup('Error', 'Problem creating account.');
       }
     },
       error => {
-        this.showPopup("Error", error);
+        this.showPopup('Error', error);
       });
   }
 
@@ -41,12 +42,12 @@ export class RegisterPage implements OnInit {
           text: 'OK',
           handler: data => {
             if (this.createSuccess) {
-              this.nav.navigateRoot('/home');
+             // this.nav.navigateRoot('/home');
             }
           }
         }
       ]
     });
     await alert.present();
-  }*/
+  }
 }
