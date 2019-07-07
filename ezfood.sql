4a651c6159368f8d669fba179ca193c8ccf6186f
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-07-07 21:20:57 +0530

    SQL

--- ezfood.sql
+++ ezfood.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 07, 2019 at 05:49 PM
+-- Generation Time: Jul 07, 2019 at 05:50 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
