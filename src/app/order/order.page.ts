befb22b06abaa91944f53f40593b11ea44508d2d
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-07-22 01:34:59 +0530

    Order functions created

--- src/app/order/order.page.ts
+++ src/app/order/order.page.ts
@@ -1,6 +1,7 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { UserService } from '../api/user.service';
+import { ConstantPool } from '@angular/compiler';
 
 @Component({
   selector: 'app-order',
@@ -10,16 +11,42 @@ import { UserService } from '../api/user.service';
 export class OrderPage implements OnInit {
 
   createSuccess = false;
-  placeOrderCredentials = { qty: '', unitprice: '', total: '', orderdate:'', ordertime:'' };
+  placeOrderCredentials = { qty: '', unitprice: '', total: '', orderdate: '', ordertime: '' };
+  cart = [];
+  tempCart = [];
+  totalPrice = 0;
 
-  constructor(private alertCtrl: AlertController, private auth: UserService) { }
+  constructor(private alertCtrl: AlertController, private auth: UserService) {
+    this.getCart();
+  }
 
   ngOnInit() {
   }
 
+  getCart() {
+    this.tempCart = [];
+    const temp__ = this.auth.getCart().then(data => {
+      this.cart = data;
+      for (let i = 0; i < this.cart.length; i++) {
+        if (this.cart[i] === '1') {
+          const tempitem1 = { name: 'Cheese Burger', price: '500' };
+          this.totalPrice += 500;
+          this.tempCart.push(tempitem1);
+        } if (this.cart[i] === '2') {
+          const tempitem2 = { name: 'Chicken Burger', price: '440' };
+          this.totalPrice += 440;
+          this.tempCart.push(tempitem2);
+        }
+      }
+    });
+  }
 
   public placeOrder() {
-    this.auth.placeOrder(this.placeOrderCredentials).subscribe(success => {
+    let temp_str = '';
+    for (let i = 0; i < this.cart.length; i++) {
+      temp_str += this.cart[i];
+    }
+    this.auth.placeOrder(temp_str).subscribe(success => {
       if (success) {
         this.createSuccess = true;
         this.showPopup('Success', 'Items Added to Cart Successfully.');
